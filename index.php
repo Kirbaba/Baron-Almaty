f45ddcf2a6f4a93b08ccefd6325107754213c5c0
Vladislav <V.kirbaba@gmail.com>
2015-07-22 15:24:09 +0300

    footer

--- index.php
+++ index.php
@@ -56,7 +56,22 @@
 
 		<footer class="footer">
 			<div class="contain">
-				
+				<div class="footer__copyr">
+					<p>1999—2015 © baron-moscow.ru</p>
+				</div>
+				<div class="footer__soc">
+					<p>Следите за нами в социальных сетях - </p>
+					<div class="soc">			
+						<a href="#nowhere"><i class="soc__fb"></i></a>
+						<a href="#nowhere"><i class="soc__vk"></i></a>
+						<a href="#nowhere"><i class="soc__tw"></i></a>
+						<a href="#nowhere"><i class="soc__ok"></i></a>
+						<a href="#nowhere"><i class="soc__yt"></i></a>
+					</div>
+				</div>
+				<div class="footer__dev">
+					<p>Разработка и поддержка сайта - Турбосайт</p>
+				</div>
 			</div>
 		</footer>
 	</div>
