e3c34f2063a19a7703787922ddfebe1a63bc3bc3
Vladislav <V.kirbaba@gmail.com>
2015-07-24 11:28:40 +0300

    category

--- page.php
+++ page.php
@@ -1,23 +1,12 @@
 <?php get_header(); ?>
     <div class="page">
-        <div class="contain">
-           
-       
-            
-
-                  
-                            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-                                <h1><?php the_title(); ?></h1>
-                                <?php the_content(); ?>
-                                <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-
-
-                            <?php endwhile; ?>
-                            <?php endif; ?>
-                 
-       
-       
+        <div class="contain"> 
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+                <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </div>
-    </div>   
+    </div>
 <?php get_footer(); ?>
