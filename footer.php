b2a7d18a1e53b2dfc0847e39b2d48a18edf0e619
Vladislav <v.kirbaba@gmail.com>
2015-07-28 16:01:37 +0300

    form

--- footer.php
+++ footer.php
@@ -21,18 +21,19 @@
 	</div>
 
     <div class="modal fade" id="callme" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><!--&times;--></button>
-            <form action="#">
-            	<input type="text" class="formPopup" placeholder="Ваше имя" required>
-            	<input type="email" class="formPopup" placeholder="Ваш e-mail" required>
-            	<textarea class="formPopupText" placeholder="Введите ваше сообщение" required></textarea>
-            	<input class="formPopupSub" type="submit" data-toggle="modal" href="#thnx" value="Отправить сообщение">
-            </form>
-            
-        </div>
-    </div>
+	    <div class="modal-dialog">
+	        <div class="modal-header">
+	            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><!--&times;--></button>
+	            <form method="POST" id="formx" action="javascript:void(null);" onsubmit="call()">
+	            	<input type="text" name="name" class="formPopup" placeholder="Ваше имя" >
+	            	<input type="email" name="email" class="formPopup" placeholder="Ваш e-mail">
+	            	<textarea class="formPopupText" name="mes" placeholder="Введите ваше сообщение"></textarea>
+	            	<input class="formPopupSub" id=""type="submit" data-toggle="modal" href="#thnx" value="Отправить сообщение">
+	            </form>
+	            
+	        </div>
+	    </div>
+	<div>
 
     <div class="modal fade" id="thnx" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
@@ -42,6 +43,7 @@
             <p><b>Ваше сообщение отправлено. <br> Наши менеджеры ответят<br>  Вам в течени 72 часов.</b></p>
         </div>
     </div>
+</div>
 </div>
 <?php wp_footer(); ?>
 </body>
